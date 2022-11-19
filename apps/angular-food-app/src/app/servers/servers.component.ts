77fbdd561ccbf95d616b64b5f010fcbb85c60ee4
FireMdu <mduduzi.mlilo@hotmail.com>
2022-11-19 12:26:16 +0200

    More event binding

--- apps/angular-food-app/src/app/servers/servers.component.ts
+++ apps/angular-food-app/src/app/servers/servers.component.ts
@@ -6,8 +6,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./servers.component.css'],
 })
 export class ServersComponent implements OnInit {
-  allowNewServer = false;
-  serverCreationStatus = 'No server was created';
+  serverName: string = '';
+  allowNewServer: boolean = false;
+  serverCreationStatus: string = 'No server was created';
 
   constructor() {
     setTimeout(() => {
@@ -20,5 +21,9 @@ export class ServersComponent implements OnInit {
 
   onCreateServer() {
     this.serverCreationStatus = 'Server was created';
+  }
+
+  onUpdateServerName(event: any) {
+    this.serverName = (<HTMLInputElement>event.target).value;
   }
 }
